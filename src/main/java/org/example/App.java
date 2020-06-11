19b321a61ddf3aa755e6023ceeb793f2146d0a25
shadow <1933541099@qq.com>
2020-06-12 03:53:32 +0800

    test connection

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,25 @@
 package org.example;
 
+import org.example.extractor.DatabaseExtractor;
+import org.example.config.ExtractConfig;
+import org.example.config.SimpleExtractConfig;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private ETLExtractor extractor;
+
+    private ExtractConfig extractconf;
+    
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	extractconf = new SimpleExtractConfig("select");
+	extractor = new DatabaseExtractor(extractconf);
+	extractor.releaseAll();
     }
+    
 }
